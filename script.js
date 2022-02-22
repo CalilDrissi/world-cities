5a359660a7ec7b104775d32b05d6e8b888476b93
Calil Drissi <calil.drissi@gmail.com>
2022-02-22 22:51:03 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const cities = [
+    "paris",
+    "london",
+    "dubai",
+    "new york",
+    "moscow",
+    "tokyo",
+    "singapore",
+    "los angeles",
+    "barcelona",
+    "madrid"
+];
+
+
+console.log(cities[2])
